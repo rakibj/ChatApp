6dcc93038a92792787338c2212cdbcbc7234ca96
Rakib <rakibjahan@iut-dhaka.edu>
2018-07-30 21:58:27 +0600

    SignUp UI Created!

--- app/src/main/java/com/apploops/rakib/chatapp/CreateUserActivity.kt
+++ app/src/main/java/com/apploops/rakib/chatapp/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.apploops.rakib.chatapp
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,14 +18,34 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        var random = Random()
+        var color = random.nextInt(2)
+        var avatar = random.nextInt(28)
 
-    }
+        userAvatar = when (color) {
+            0 -> "light$avatar"
+            else -> "dark$avatar"
+        }
 
-    fun createUserClicked(view: View) {
-
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+    }
+
+    fun createUserClicked(view: View) {
 
     }
 }
