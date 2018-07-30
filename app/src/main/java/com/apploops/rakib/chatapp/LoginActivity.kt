5c6aad0da61c4132629af71abea3401207a16da9
unknown <rakibjahan@iut-dhaka.edu>
2018-07-30 20:45:00 +0600

    Login UI Created!

---
+++ app/src/main/java/com/apploops/rakib/chatapp/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.apploops.rakib.chatapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserClicked(view: View) {
+
+    }
+
+    fun loginLoginButtonClicked(view: View) {
+
+    }
+}
